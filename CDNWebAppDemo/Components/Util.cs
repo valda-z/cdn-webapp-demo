f15f4af19c8b4627c50f0ca50e7508a278c9f48e
valda-z <valdemar.zavadsky@microsoft.com>
2017-04-27 21:39:12 +0200

    first commit with skeleton

---
+++ CDNWebAppDemo/Components/Util.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CDNWebAppDemo.Components
+{
+    public static class Util
+    {
+        public static string GetCDNURL()
+        {
+            return CDNWebAppDemo.Properties.Settings.Default.CDNURL;
+        }
+    }
+}
---
+++ CDNWebAppDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CDNWebAppDemo.MvcApplication" Language="C#" %>
