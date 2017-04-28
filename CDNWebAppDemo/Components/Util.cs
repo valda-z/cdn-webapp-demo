a9bb4521b62c28ced8d5bebde6766f6c9bf6161c
valda-z <valdemar.zavadsky@microsoft.com>
2017-04-28 09:01:54 +0200

    demo ready

--- CDNWebAppDemo/Components/Util.cs
+++ CDNWebAppDemo/Components/Util.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 
@@ -9,7 +10,7 @@ namespace CDNWebAppDemo.Components
     {
         public static string GetCDNURL()
         {
-            return CDNWebAppDemo.Properties.Settings.Default.CDNURL;
+            return ConfigurationManager.AppSettings["CDNURL"];
         }
     }
 }
---
+++ CDNWebAppDemo/Models/ImageHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CDNWebAppDemo.Models
+{
+    public static class ImageHelper
+    {
+        public static MvcHtmlString Image(this HtmlHelper helper, string src, string altText, string width, string height)
+        {
+            var builder = new TagBuilder("img");
+            builder.MergeAttribute("src", src);
+            builder.MergeAttribute("alt", altText);
+            builder.MergeAttribute("width", width);
+            builder.MergeAttribute("height", height);
+            return MvcHtmlString.Create(builder.ToString(TagRenderMode.SelfClosing));
+        }
+    }
+}
